60f3d6c3442e3b5482014dd3a36f7245a2d87b44
chadwalt <chadwalt04@gmail.com>
2018-11-28 08:17:41 +0300

    Enable file system on the app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -48,6 +48,15 @@ $app->singleton(
     App\Console\Kernel::class
 );
 
+$app->singleton(
+    Illuminate\Contracts\Filesystem\Factory::class,
+    function ($app) {
+        return new Illuminate\Filesystem\FilesystemManager($app);
+    }
+);
+
+$app->configure('filesystems');
+
 /*
 |--------------------------------------------------------------------------
 | Register Middleware
